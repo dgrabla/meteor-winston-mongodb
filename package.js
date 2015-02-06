8c43386346dda79447f90257867833fed9abb7b1
lfergon <lfergon@gmail.com>
2015-02-06 12:37:54 +0100

    Changes on package for new packages api

--- package.js
+++ package.js
@@ -1,15 +1,28 @@
 Package.describe({
-    summary: 'Exposes winston.transports.MongoDB (https://github.com/indexzero/winston-mongodb)'
+  summary: 'Exposes winston.transports.MongoDB (https://github.com/indexzero/winston-mongodb)',
+  version: "0.0.2",
+  name: "dgrabla:meteor-winston-mongodb",
+  git: "https://github.com/dgrabla/meteor-winston-mongodb"
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('0.1.0');
+  api.add_files('winston-mongodb.js', 'server');
+  if (api.export) {
+    api.export("MongoDB", 'server');
+  }
+});
+
+Package.onTest(function (api) {
+  // Sets up a dependency on this package
+  api.use('dgrabla:meteor-winston-mongodb');
+  // Allows you to use the 'tinytest' framework
+  api.use('tinytest@1.0.0');
+  // Specify the source code for the package tests
+  api.addFiles('winston-mongodb.js', 'server');
 });
 
 Npm.depends({
-    "winston": "0.7.2",
-    "winston-mongodb": "0.4.0"
-});
-
-Package.on_use(function (api) {
-    api.add_files('winston-mongodb.js', 'server');
-    if (api.export) {
-        api.export("MongoDB", 'server');
-    };
+  "winston": "0.9.0",
+  "winston-mongodb": "0.5.3"
 });
